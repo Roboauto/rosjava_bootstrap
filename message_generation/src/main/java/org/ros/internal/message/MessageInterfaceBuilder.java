5ed1d5d6cfd53a1260420ebb67688b2af3d52a35
Pavel Černocký <pavel.cernocky@artin.cz>
2017-08-10 12:03:03 +0200

    generate message implementation classes

--- message_generation/src/main/java/org/ros/internal/message/MessageInterfaceBuilder.java
+++ message_generation/src/main/java/org/ros/internal/message/MessageInterfaceBuilder.java
@@ -18,18 +18,37 @@ package org.ros.internal.message;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Sets;
-
+import com.squareup.javapoet.ArrayTypeName;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
+import io.netty.buffer.ByteBuf;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.ros.exception.RosMessageRuntimeException;
 import org.ros.internal.message.context.MessageContext;
 import org.ros.internal.message.context.MessageContextProvider;
+import org.ros.internal.message.field.BooleanArrayField;
+import org.ros.internal.message.field.ByteBufField;
+import org.ros.internal.message.field.DoubleArrayField;
 import org.ros.internal.message.field.Field;
 import org.ros.internal.message.field.FieldType;
+import org.ros.internal.message.field.FloatArrayField;
+import org.ros.internal.message.field.IntegerArrayField;
+import org.ros.internal.message.field.ListField;
+import org.ros.internal.message.field.LongArrayField;
 import org.ros.internal.message.field.MessageFields;
 import org.ros.internal.message.field.PrimitiveFieldType;
+import org.ros.internal.message.field.ShortArrayField;
 import org.ros.message.MessageDeclaration;
 import org.ros.message.MessageFactory;
 
+import javax.lang.model.element.Modifier;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -190,4 +209,122 @@ public class MessageInterfaceBuilder {
       builder.append(String.format("  void %s(%s value);\n", setter, type));
     }
   }
+
+  public String buildClass(MessageFactory messageFactory) {
+    Preconditions.checkNotNull(messageDeclaration);
+    Preconditions.checkNotNull(interfaceName);
+
+    TypeSpec.Builder classBuilder = TypeSpec.classBuilder(interfaceName + "Impl")
+      .addModifiers(Modifier.PUBLIC)
+      .addSuperinterface(ClassName.get(packageName, interfaceName));
+
+    MessageContextProvider messageContextProvider = new MessageContextProvider(messageFactory);
+    MessageContext messageContext = messageContextProvider.get(messageDeclaration);
+
+    MessageFields messageFields = new MessageFields(messageContext);
+    Set<String> getters = Sets.newHashSet();
+    for (Field field : messageFields.getFields()) {
+      if (field.isConstant()) {
+        continue;
+      }
+      String getter = messageContext.getFieldGetterName(field.getName());
+      String setter = messageContext.getFieldSetterName(field.getName());
+      if (getters.contains(getter)) {
+        // In the case that two or more message fields have the same name except
+        // for capitalization, we only generate a getter and setter pair for the
+        // first one. The following fields will only be accessible via the
+        // RawMessage interface.
+        continue;
+      }
+      getters.add(getter);
+
+      TypeName fieldType = getFieldTypeName(field);
+      FieldSpec fieldSpec = FieldSpec.builder(fieldType, field.getName(), Modifier.PRIVATE).build();
+      classBuilder.addField(fieldSpec);
+      classBuilder.addMethod(
+        MethodSpec.methodBuilder(getter)
+          .addModifiers(Modifier.PUBLIC)
+          .addAnnotation(Override.class)
+          .returns(fieldType)
+          .addStatement("return $N", fieldSpec)
+          .build()
+      );
+
+      ParameterSpec paramSpec;
+      classBuilder.addMethod(
+        MethodSpec.methodBuilder(setter)
+          .addModifiers(Modifier.PUBLIC)
+          .addAnnotation(Override.class)
+          .addParameter(paramSpec = ParameterSpec.builder(fieldType, field.getName()).build())
+          .addStatement("this.$N = $N", fieldSpec, paramSpec)
+          .build()
+      );
+    }
+
+    classBuilder.addMethod(
+      MethodSpec.methodBuilder("toRawMessage")
+        .addModifiers(Modifier.PUBLIC)
+        .addAnnotation(Override.class)
+        .returns(ClassName.get(RawMessage.class))
+        .addStatement("throw new $T()", UnsupportedOperationException.class)
+        .build()
+    );
+
+    JavaFile javaFile = JavaFile.builder(packageName, classBuilder.build()).build();
+    return javaFile.toString();
+  }
+
+  private TypeName getFieldTypeName(Field field) {
+    if (field instanceof BooleanArrayField) {
+      return ArrayTypeName.of(boolean.class);
+    }
+    if (field instanceof ByteBufField) {
+      return ClassName.get(ByteBuf.class);
+    }
+    if (field instanceof ShortArrayField) {
+      return ArrayTypeName.of(short.class);
+    }
+    if (field instanceof IntegerArrayField) {
+      return ArrayTypeName.of(int.class);
+    }
+    if (field instanceof LongArrayField) {
+      return ArrayTypeName.of(long.class);
+    }
+    if (field instanceof FloatArrayField) {
+      return ArrayTypeName.of(float.class);
+    }
+    if (field instanceof DoubleArrayField) {
+      return ArrayTypeName.of(double.class);
+    }
+
+    String javaTypeName = field.getType().getJavaTypeName();
+
+    if (field instanceof ListField) {
+      return ParameterizedTypeName.get(ClassName.get(List.class), ClassName.bestGuess(javaTypeName));
+    }
+
+    if (javaTypeName.equals("float")) {
+      return TypeName.FLOAT;
+    }
+    if (javaTypeName.equals("long")) {
+      return TypeName.LONG;
+    }
+    if (javaTypeName.equals("boolean")) {
+      return TypeName.BOOLEAN;
+    }
+    if (javaTypeName.equals("double")) {
+      return TypeName.DOUBLE;
+    }
+    if (javaTypeName.equals("short")) {
+      return TypeName.SHORT;
+    }
+    if (javaTypeName.equals("byte")) {
+      return TypeName.BYTE;
+    }
+    if (javaTypeName.equals("int")) {
+      return TypeName.INT;
+    }
+    return ClassName.bestGuess(javaTypeName);
+  }
+
 }
